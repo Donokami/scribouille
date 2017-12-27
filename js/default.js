e3e415be804bfd637f81c5961285e4cfbcbaf33d
Kiyoshi <Donokami@users.noreply.github.com>
2017-12-27 23:27:40 +0100

    Mise à jour du lien.

--- js/default.js
+++ js/default.js
@@ -7,7 +7,7 @@ C\'est une sorte d\'éditeur de texte simple pour se concentrer sur l\'écriture
 	Pour commencer, supprimez tout ce texte et remplissez cette page de jolis petits mots. Vous verrez, c\'est simple.  \
 </p> \
 <p> \
-	Vous pouvez écrire en <b>gras</b>, en <i>italique</i>, <b><i>les deux combinés</i></b> et même mettre des <a href="https://kiyo.ooo/s"> urls </a> juste en surlignant le texte et en sélectionnant ce qu\'il vous plaît.\
+	Vous pouvez écrire en <b>gras</b>, en <i>italique</i>, <b><i>les deux combinés</i></b> et même mettre des <a href="https://kiyo.ooo/s">urls</a> juste en surlignant le texte et en sélectionnant ce qu\'il vous plaît.\
 </p>\
 <blockquote>\
 	Vous pouvez même mettre des citations !\
